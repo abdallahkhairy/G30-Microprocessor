373563e42e40648c3083cc46acd478ccc9cb2e71
abdullahshaker10 <abdullah.shaker2017@gmail.com>
2019-03-12 01:20:54 +0200

    Update Port_SetPinPullDown.c

--- Abdullah shaker/Port_SetPinPullDown.c
+++ Abdullah shaker/Port_SetPinPullDown.c
@@ -9,23 +9,23 @@ void Port_SetPinPullDown ( uint8 port_index , uint8 pins_mask , uint8 enable )
 	break;
 	
 	case 1:
-	(enable==0)? : GPIO_PORTA_PDR_R &= ~pins_mask : GPIO_PORTB_PDR_R |= pins_mask;
+	(enable==0)? : GPIO_PORTB_PDR_R &= ~pins_mask : GPIO_PORTB_PDR_R |= pins_mask;
 	break;
 	
 	case 2:
-	(enable==0)? : GPIO_PORTA_PDR_R &= ~pins_mask : GPIO_PORTC_PDR_R |= pins_mask;
+	(enable==0)? : GPIO_PORTC_PDR_R &= ~pins_mask : GPIO_PORTC_PDR_R |= pins_mask;
 	break;
 	
 	case 3:
-	(enable==0)? : GPIO_PORTA_PDR_R &= ~pins_mask : GPIO_PORTD_PDR_R |= pins_mask;
+	(enable==0)? : GPIO_PORTD_PDR_R &= ~pins_mask : GPIO_PORTD_PDR_R |= pins_mask;
 	break;
 	
 	case 4:
-	(enable==0)? : GPIO_PORTA_PDR_R &= ~pins_mask : GPIO_PORTE_PDR_R |= pins_mask;
+	(enable==0)? : GPIO_PORTE_PDR_R &= ~pins_mask : GPIO_PORTE_PDR_R |= pins_mask;
 	break;
 	
 	case 5:
-	(enable==0)? : GPIO_PORTA_PDR_R &= ~pins_mask : GPIO_PORTF_PDR_R |= pins_mask;
+	(enable==0)? : GPIO_PORTF_PDR_R &= ~pins_mask : GPIO_PORTF_PDR_R |= pins_mask;
 	break;
 	}	
 }
