29de48ee33e36a10129e03b216eb11088845d2b4
Abdelrahman Alwali <a.alwali96@gmail.com>
2019-03-11 20:48:45 +0200

    Add files via upload

---
+++ Abdelrahman Al-Wali/DIO/DIO_FlipPort_Function/DIO_FlipPort_Function.c
@@ -0,0 +1,27 @@
+
+
+
+void DIO_FlipPort ( u8 port_index , u8 pins_mask )
+{
+	switch(port_index)
+	{
+		case GPIO_PORT_A
+		GPIO_PORTA_DATA_R ^= pins_mask;
+		break;
+		case GPIO_PORT_B
+		GPIO_PORTB_DATA_R ^= pins_mask;
+		break;
+		case GPIO_PORT_C
+		GPIO_PORTC_DATA_R ^= pins_mask;
+		break;
+		case GPIO_PORT_D
+		GPIO_PORTD_DATA_R ^= pins_mask;
+		break;
+		case GPIO_PORT_E
+		GPIO_PORTE_DATA_R ^= pins_mask;
+		break;
+		case GPIO_PORT_F
+		GPIO_PORTF_DATA_R ^= pins_mask;
+		break;
+	}
+}
