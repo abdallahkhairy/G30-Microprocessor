89d38ca54b98953384ae2ec1f4ff7c8caefda8e9
Abdullah Khairy <abdullahkhairy98@gmail.com>
2019-03-12 13:30:30 +0200

    Create Port_Init.c

---
+++ Abdullah Khairy/Port_Init.c
@@ -0,0 +1,7 @@
+#define PORTA      0x00000001 
+#define PORTB      0x00000002
+#define PORTC      0x00000004 
+#define PORTD      0x00000008 
+#define PORTE      0x00000010 
+#define PORTF      0x00000020                                
+                               
