f5d7cae1295efa0d0052c8a2f8235186658ea074
Abdelrahman Alwali <a.alwali96@gmail.com>
2019-03-12 13:54:13 +0200

    Update Port_SetPinPullDown.c

--- Abdullah shaker/Port_SetPinPullDown.c
+++ Abdullah shaker/Port_SetPinPullDown.c
@@ -4,27 +4,27 @@ void Port_SetPinPullDown ( uint8 port_index , uint8 pins_mask , uint8 enable )
 {
 	switch(port_index)
 	{
-	case 0:
+	case GPIO_PORT_A:
 	(enable==0)? : GPIO_PORTA_PDR_R &= ~pins_mask : GPIO_PORTA_PDR_R |= pins_mask;
 	break;
 	
-	case 1:
+	case GPIO_PORT_B:
 	(enable==0)? : GPIO_PORTB_PDR_R &= ~pins_mask : GPIO_PORTB_PDR_R |= pins_mask;
 	break;
 	
-	case 2:
+	case GPIO_PORT_C:
 	(enable==0)? : GPIO_PORTC_PDR_R &= ~pins_mask : GPIO_PORTC_PDR_R |= pins_mask;
 	break;
 	
-	case 3:
+	case GPIO_PORT_D:
 	(enable==0)? : GPIO_PORTD_PDR_R &= ~pins_mask : GPIO_PORTD_PDR_R |= pins_mask;
 	break;
 	
-	case 4:
+	case GPIO_PORT_E:
 	(enable==0)? : GPIO_PORTE_PDR_R &= ~pins_mask : GPIO_PORTE_PDR_R |= pins_mask;
 	break;
 	
-	case 5:
+	case GPIO_PORT_F:
 	(enable==0)? : GPIO_PORTF_PDR_R &= ~pins_mask : GPIO_PORTF_PDR_R |= pins_mask;
 	break;
 	}	
