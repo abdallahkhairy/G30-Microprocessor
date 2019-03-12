028eba421e5d130d2c8a59251112bdc868b0be43
Abdelrahman Alwali <a.alwali96@gmail.com>
2019-03-12 13:40:11 +0200

    Update Port_SetPinPullUp.c

--- Abdullah Khairy/Port_SetPinPullUp.c
+++ Abdullah Khairy/Port_SetPinPullUp.c
@@ -1,44 +1,39 @@
-#define PORTA      0x00000001 
-#define PORTB      0x00000002
-#define PORTC      0x00000004 
-#define PORTD      0x00000008 
-#define PORTE      0x00000010 
-#define PORTF      0x00000020 
+
 
 void Port_SetPinPullUp(uint8 port_index , uint8 pins_mask , uint8 enable){
 	switch(port_index)
 	{
-		case PORTA:
+		case GPIO_PORT_A:
 			if(enable)
 				GPIO_PORTA_PUR_R |= pins_mask;
 			else 
 				GPIO_PORTA_PUR_R &= ~pins_mask;
 			break;
-		case PORTB:
+		case GPIO_PORT_B:
 			if(enable)
 				GPIO_PORTB_PUR_R |= pins_mask;
 			else 
 				GPIO_PORTB_PUR_R &= ~pins_mask;
 			break;
-		case PORTC:
+		case GPIO_PORT_C:
 			if(enable)
 				GPIO_PORTC_PUR_R |= pins_mask;
 			else 
 				GPIO_PORTC_PUR_R &= ~pins_mask;
 			break;		
-		case PORTD:
+		case GPIO_PORT_D:
 			if(enable)
 				GPIO_PORTD_PUR_R |= pins_mask;
 			else 
 				GPIO_PORTD_PUR_R &= ~pins_mask;
 			break;		
-		case PORTE:
+		case GPIO_PORT_E:
 			if(enable)
 				GPIO_PORTE_PUR_R |= pins_mask;
 			else 
 				GPIO_PORTE_PUR_R &= ~pins_mask;
 			break;		
-		case PORTF:
+		case GPIO_PORT_F:
 			if(enable)
 				GPIO_PORTF_PUR_R |= pins_mask;
 			else 
