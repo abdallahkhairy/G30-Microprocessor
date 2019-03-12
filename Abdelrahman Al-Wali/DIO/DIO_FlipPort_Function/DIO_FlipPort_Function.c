a4c1b55a2fd170e04c86511a977dc60bb09ace86
Abdullah Khairy <abdullahkhairy98@gmail.com>
2019-03-12 13:50:29 +0200

    Update DIO_FlipPort_Function.c

--- Abdelrahman Al-Wali/DIO/DIO_FlipPort_Function/DIO_FlipPort_Function.c
+++ Abdelrahman Al-Wali/DIO/DIO_FlipPort_Function/DIO_FlipPort_Function.c
@@ -5,22 +5,22 @@ void DIO_FlipPort ( uint8 port_index , uint8 pins_mask )
 {
 	switch(port_index)
 	{
-		case GPIO_PORT_A
+		case GPIO_PORT_A :
 		GPIO_PORTA_DATA_R ^= pins_mask;
 		break;
-		case GPIO_PORT_B
+		case GPIO_PORT_B :
 		GPIO_PORTB_DATA_R ^= pins_mask;
 		break;
-		case GPIO_PORT_C
+		case GPIO_PORT_C :
 		GPIO_PORTC_DATA_R ^= pins_mask;
 		break;
-		case GPIO_PORT_D
+		case GPIO_PORT_D :
 		GPIO_PORTD_DATA_R ^= pins_mask;
 		break;
-		case GPIO_PORT_E
+		case GPIO_PORT_E :
 		GPIO_PORTE_DATA_R ^= pins_mask;
 		break;
-		case GPIO_PORT_F
+		case GPIO_PORT_F :
 		GPIO_PORTF_DATA_R ^= pins_mask;
 		break;
 	}
