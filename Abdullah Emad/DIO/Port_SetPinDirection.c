f10aa59c2288381eb4afe1c704590a1c350045fc
AbdullahHajjaj <36624498+AbdullahHajjaj@users.noreply.github.com>
2019-03-15 20:24:21 +0200

    Update Port_SetPinDirection.c

--- Abdullah Emad/DIO/Port_SetPinDirection.c
+++ Abdullah Emad/DIO/Port_SetPinDirection.c
@@ -4,82 +4,67 @@ void Port_SetPinDirection(unit8 port_index, unit8 pins_mask, Port_PinDirectionTy
 	switch (port_index)
 	{
 
-	case GPIO_PORTA:
+		case GPIO_PORTA:
+			if (Port_PinDirectionType)
+			{
+				GPIO_PORTA_DIR_R |= pins_mask;
+			}
+			else
+			{
+				GPIO_PORTA_DIR_R &= ~pins_mask;
+			}
+			break;
 
-		if (Port_PinDirectionType)
-		{
-			GPIO_PORTA_DIR_R |= pins_mask;
+		case GPIO_PORTB:
+			if (Port_PinDirectionType)
+			{
+				GPIO_PORTB_DIR_R |= pins_mask;
+			}
+			else
+			{
+				GPIO_PORTB_DIR_R &= ~pins_mask;
+			}
+			break;
+
+		case GPIO_PORTC:
+			if (Port_PinDirectionType)
+			{
+				GPIO_PORTC_DIR_R |= pins_mask;
+			}
+			else
+			{
+				GPIO_PORTC_DIR_R &= ~pins_mask;
+			}
+			break;
+		case GPIO_PORTD:
+			if (Port_PinDirectionType)
+			{
+				GPIO_PORTD_DIR_R |= pins_mask;
+			}
+			else
+			{
+				GPIO_PORTD_DIR_R &= ~pins_mask;
+			}
+			break;
+		case GPIO_PORTE:
+			if (Port_PinDirectionType)
+			{
+				GPIO_PORTE_DIR_R |= pins_mask;
+			}
+			else
+			{
+				GPIO_PORTE_DIR_R &= ~pins_mask;
+			}
+			break;
+		case GPIO_PORTF:
+			if (Port_PinDirectionType)
+			{
+				GPIO_PORTF_DIR_R |= pins_mask;
+			}
+			else
+			{
+				GPIO_PORTF_DIR_R &= ~pins_mask;
+			}
+			break;
 		}
-		else
-		{
-			GPIO_PORTA_DIR_R &= ~pins_mask;
-		}
-
-		break;
-
-	case GPIO_PORTB:
-
-		if (Port_PinDirectionType)
-		{
-			GPIO_PORTB_DIR_R |= pins_mask;
-		}
-		else
-		{
-			GPIO_PORTB_DIR_R &= ~pins_mask;
-		}
-
-		break;
-
-	case GPIO_PORTC:
-
-		if (Port_PinDirectionType)
-		{
-			GPIO_PORTC_DIR_R |= pins_mask;
-		}
-		else
-		{
-			GPIO_PORTC_DIR_R &= ~pins_mask;
-		}
-
-		break;
-
-	case GPIO_PORTD:
-
-		if (Port_PinDirectionType)
-		{
-			GPIO_PORTD_DIR_R |= pins_mask;
-		}
-		else
-		{
-			GPIO_PORTD_DIR_R &= ~pins_mask;
-		}
-
-		break;
-
-	case GPIO_PORTE:
-
-		if (Port_PinDirectionType)
-		{
-			GPIO_PORTE_DIR_R |= pins_mask;
-		}
-		else
-		{
-			GPIO_PORTE_DIR_R &= ~pins_mask;
-		}
-
-		break;
-
-	case GPIO_PORTF:
-
-		if (Port_PinDirectionType)
-		{
-			GPIO_PORTF_DIR_R |= pins_mask;
-		}
-		else
-		{
-			GPIO_PORTF_DIR_R &= ~pins_mask;
-		}
-
-		break;
-	}
 }
