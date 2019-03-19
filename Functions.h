c7d11ec94e039aae9ec9bffa99078b25df0edb10
Abdullah Khairy <abdullahkhairy98@gmail.com>
2019-03-19 22:37:39 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -18,7 +18,8 @@ void SevSegment2_DisplayNo( uint8 No ); // to display certain number on seven se
 void SevSegment3_DisplayNo( uint8 No ); // to display certain number on seven segment 3
 void Delay_Ms(uint32_t Num); // to make some delay in milliseconds but it not very accurate
 void LCD_clear(); // to clear lcd 
-void LCD_Home(); // when the new character is not visible on the display immediately. The user must call, one of the funtions lcd.home()or lcd.setCursor()after the character creation.
+void LCD_Home(); /* when the new character is not visible on the display immediately. The user must call, 
+one of the funtions lcd.home()or lcd.setCursor()after the character creation. */
 void LCD_writeString(u8 *ptr, u8 xpos, u8 ypos); // to write words on the lcd
 void CounterTO999(void); // the application of counter by first method 
  /******************** Abdelrahman Al-Wali ***************************/
