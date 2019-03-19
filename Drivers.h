7ce4810673b6986fa9a620623fe1c416ba56ab1a
Abdullah Khairy <abdullahkhairy98@gmail.com>
2019-03-19 20:13:11 +0200

    Update Drivers.h

--- Drivers.h
+++ Drivers.h
@@ -3,7 +3,7 @@ typedef unsigned char uint8;
 void PortInit(uint8 port_index );
 // To Initlize The Selected Port By Enabling The clk And make It Digital And Unlock it
 void Port_SetPinPullUp(uint8 port_index , uint8 pins_mask , uint8 enable);
-// To make an intrnal restitance to make it high when no press and low when press
+// To Make an Intrnal Resistance to Make it High When no Press and Low When Press
 #include"Switches.h"
 // switches driver
 
