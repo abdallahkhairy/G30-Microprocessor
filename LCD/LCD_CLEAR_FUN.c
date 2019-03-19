acaecf352df488426bd42807edbe56ddfb69d6f3
Abdelrahman Alwali <a.alwali96@gmail.com>
2019-03-19 20:04:22 +0200

    Add files via upload

---
+++ LCD/LCD_CLEAR_FUN.c
@@ -0,0 +1,17 @@
+
+
+
+void LCD_clear()
+{
+	DIO_WritePort(LCD_CONTROL_PINS_PORT,RS,STD_LOW);
+	DIO_WritePort(LCD_CONTROL_PINS_PORT,RW,STD_LOW);
+	DIO_WritePort(LCD_PORT,0x01,STD_HIGH);
+	DIO_WritePort(LCD_PORT,0xFE,STD_LOW);
+	/* set ENABLE high */
+	DIO_WritePort(LCD_CONTROL_PINS_PORT,E,STD_HIGH);;
+	/* Delay 1 ms */
+	Delay_Ms(1);
+	/*set Enable LOW */
+	DIO_WritePort(LCD_CONTROL_PINS_PORT,E,STD_LOW);
+	Delay_Ms(1);
+}
