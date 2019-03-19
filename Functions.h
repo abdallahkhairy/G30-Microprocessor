a7791b84aa594f33bd5bfd8647edcd8265135a33
Abdullah Khairy <abdullahkhairy98@gmail.com>
2019-03-19 20:55:06 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -1,6 +1,6 @@
 /************************ Abdullah Khairy ************************/
-typedef unsigned int uint32_t;
-typedef unsigned char uint8;
+typedef unsigned int uint32_t; // Using uint32_t Instead Of int
+typedef unsigned char uint8; // Using uint8 Instead Of char
 void PortInit(uint8 port_index );// To Initlize The Selected Port By Enabling The clk And make It Digital And Unlock it
 void Port_SetPinPullUp(uint8 port_index , uint8 pins_mask , uint8 enable);// To Make an Intrnal Resistance to Make it High When no Press and Low When Press
 #include"Switches.h"// switches driver
