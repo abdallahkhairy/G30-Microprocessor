2b249b4dd01875fe51d4359590485614f602ef68
Abdullah Khairy <abdullahkhairy98@gmail.com>
2019-03-20 01:15:21 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -23,38 +23,35 @@ void LCD_Home(); /* when the new character is not visible on the display immedia
 one of the funtions lcd.home()or lcd.setCursor()after the character creation. */
 void LCD_writeString(u8 *ptr, u8 xpos, u8 ypos); // to write words on the lcd
 void CounterTO999(void); // the application of counter by first method 
- /******************** Abdelrahman Al-Wali *************************/
- /*
+/******************** Abdelrahman Al-Wali *************************/
+ 
 
-  /********************* Abdullah Emad ****************************/
+/********************* Abdullah Emad ******************************/
 void lcd_FunctionSet8bit(void); //to run the LCD in 16*2 mode
-
 void LCDcommand(uint8 cmd);// to send command to LCD
-
 void LCD_Init(); // to initiat the LCD 
+void DIO_WritePort(unit8 port_index,unit8 pins_mask, Dio_LevelType pins_level); /*To have an output 1 or 0 
+in a certain pin using a pin mask*/
+void Port_SetPinDirection(unit8 port_index, unit8 pins_mask, Port_PinDirectionType pins_direction); /*to set the direction  
+of a certain bit thatit's input or output*/
+ /********************* Abdullah Emad ******************************/
 
-void DIO_WritePort(unit8 port_index,unit8 pins_mask, Dio_LevelType pins_level); //To have an output 1 or 0 in a certain pin using a pin mask
-
-void Port_SetPinDirection(unit8 port_index, unit8 pins_mask, Port_PinDirectionType pins_direction); //to set the direction of a certain bit that it's input or output
-
- /********************* Abdullah Emad ****************************/
 
  /********************* Abdullah shaker ****************************/
 void LCD_set_cursor(uint8 xpos, uint8 ypos); // that move the cursor to specific position 
 void LCD_DATA(u8 data); // that send data to show it in LCD 
-void Port_SetPinPullDown ( uint8 port_index , uint8 pins_mask , uint8 enable ); // to config the pulldown mode on the pins selected
+void Port_SetPinPullDown ( uint8 port_index , uint8 pins_mask , uint8 enable ); /* to config the pulldown mode on 
+the pins selected*/
 void LCD_CLR(void); // to clear lcd  
  /********************* Abdullah shaker ****************************/
  
  
-   /********************* Abdullah Fathy ****************************/
-uint8 DIO_ReadPort(uint8 port_index,pins_mask);//to read the the iput signal of selected pins of a specific port  denpends on mask received
- 
- void lcd_DisplayOff(void);//to turn off lcd
- 
+ /*********************** Abdullah Fathy ***************************/
+uint8 DIO_ReadPort(uint8 port_index,pins_mask);/*to read the the iput signal of selected pins of a specific port 
+denpends on mask received*/
+void lcd_DisplayOff(void);//to turn off lcd 
 void lcd_Entrymode(void);// to be able to write on lcd 
- 
- /********************* Abdullah Fathy ****************************/
+ /*********************** Abdullah Fathy ****************************/
  
  
 
