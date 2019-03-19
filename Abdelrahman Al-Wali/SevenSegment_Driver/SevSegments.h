aef6df52d48bf47945cc2eae3e0eda5335e12580
Abdelrahman Alwali <a.alwali96@gmail.com>
2019-03-19 23:35:53 +0200

    Update SevSegments.h

--- Abdelrahman Al-Wali/SevenSegment_Driver/SevSegments.h
+++ Abdelrahman Al-Wali/SevenSegment_Driver/SevSegments.h
@@ -1,13 +1,8 @@
 #ifndef SEVSEGMENTS_H_
 #define SEVSEGMENTS_H_
 
+#include "STD_TYPES.h"
 
-void SevSegment1_Enable();
-void SevSegment2_Enable();
-void SevSegment3_Enable();
-void SevSegment1_Disable();
-void SevSegment2_Disable();
-void SevSegment3_Disable();
 void SevSegment1_DisplayNo( uint8 No );
 void SevSegment2_DisplayNo( uint8 No );
 void SevSegment3_DisplayNo( uint8 No );
