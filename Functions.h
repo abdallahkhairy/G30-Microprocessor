210e1bcf8da590012e0dce4bef3a1ab860fd4bf4
Abdelrahman Alwali <a.alwali96@gmail.com>
2019-03-19 20:38:18 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -1,3 +1,4 @@
+/*****************************************************************/
 typedef unsigned int uint32_t;
 typedef unsigned char uint8;
 void PortInit(uint8 port_index );
@@ -10,5 +11,19 @@ void lcd_DisplayOn(void);
 // Wake Up The LCD 
 void lcd_FunctionReset(void)
  // Back it To The Defult Setting
- /********************* Abdullah Khairy***************************/
+ /********************* Abdullah Khairy ***************************/
+ 
+ 
+/*****************************************************************/
+void DIO_FlipPort ( uint8 port_index , uint8 pins_mask ); // to Toggle selected pins depend on mask recqieved
+void SevSegment1_DisplayNo( uint8 No ); // to display certain number on seven segment 1
+void SevSegment2_DisplayNo( uint8 No ); // to display certain number on seven segment 2
+void SevSegment3_DisplayNo( uint8 No ); // to display certain number on seven segment 3
+void Delay_Ms(uint32_t Num); // to make some delay in milliseconds but it not very accurate
+void Delay_Us(uint32_t Num); // to make some delay in milliseconds but it not very accurate
+void LCD_clear(); // to clear lcd 
+void LCD_Home(); // when the new character is not visible on the display immediately. The user must call, one of the funtions lcd.home()or lcd.setCursor()after the character creation.
+void LCD_writeString(u8 *ptr, u8 xpos, u8 ypos); // to write words on the lcd
+void CounterTO999(void); // the application of counter by first method 
+ /*********************Abdelrahman Al-Wali ***************************/
 
