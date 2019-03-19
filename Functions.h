dfa7155b44a7e75dc3b7c0027dfa5608e75881b3
Abdullah Khairy <abdullahkhairy98@gmail.com>
2019-03-19 20:47:04 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -8,6 +8,7 @@ void Port_SetPinPullUp(uint8 port_index , uint8 pins_mask , uint8 enable);
 #include"Switches.h"
 // switches driver
 void lcd_DisplayOn(void);
+void Delay_Us(uint32_t Num); // to make some delay in milliseconds but it not very accurate
 // Wake Up The LCD 
 void lcd_FunctionReset(void)
  // Back it To The Defult Setting
@@ -20,7 +21,6 @@ void SevSegment1_DisplayNo( uint8 No ); // to display certain number on seven se
 void SevSegment2_DisplayNo( uint8 No ); // to display certain number on seven segment 2
 void SevSegment3_DisplayNo( uint8 No ); // to display certain number on seven segment 3
 void Delay_Ms(uint32_t Num); // to make some delay in milliseconds but it not very accurate
-void Delay_Us(uint32_t Num); // to make some delay in milliseconds but it not very accurate
 void LCD_clear(); // to clear lcd 
 void LCD_Home(); // when the new character is not visible on the display immediately. The user must call, one of the funtions lcd.home()or lcd.setCursor()after the character creation.
 void LCD_writeString(u8 *ptr, u8 xpos, u8 ypos); // to write words on the lcd
